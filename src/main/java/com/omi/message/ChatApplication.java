fbf952a280bfe70b40b9a74fa155c404790977c2
HaiTV <haitv@ominext.com>
2020-03-04 23:02:04 +0700

    init server

---
+++ src/main/java/com/omi/message/ChatApplication.java
@@ -0,0 +1,13 @@
+package com.omi.message;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatApplication.class, args);
+	}
+
+}
